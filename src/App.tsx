c3204855b077f17fc11581c8419ab462014843c7
srashee1 <saad.a.rasheed@accenture.com>
2021-10-15 12:11:08 -0500

    Alerts printing to screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,50 @@ import { w3cwebsocket as W3CWebSocket } from "websocket";
 
 const client = new W3CWebSocket('ws://alerts-api.staging.internal.smartcolumbusos.com')
 
+interface Alert {
+  id: string
+  type: AlertType
+  severity: AlertSeverity
+  time: string
+  coordinates: Coordinates
+  roadName: string
+  status: AlertStatus
+}
+interface Coordinates {
+  latitude: number
+  longitude: number
+}
+
+export enum AlertStatus {
+  NEW = 'new',
+}
+
+export enum AlertType {
+  CONGESTION = 'congestion',
+}
+
+export enum AlertSeverity {
+  WARN = 'warn',
+}
 export default class App extends Component {
+
+  state = {
+    alerts: []
+  }
 
   componentDidMount() {
     client.onopen = () => {
       console.log('Connected to Alerting Engine');
     };
 
-    client.onmessage = (event) => {
-      this.setState({ currentData: JSON.parse((event.data).toString()) });
-      console.log('Received Alert')
+    client.onmessage = (message) => {
+      const alert = message.data as string;
+      if (alert !== "Connected") {
+        const parsedAlert = JSON.parse(alert) as Alert;
+        console.log('Received Alert');
+        console.log(parsedAlert.coordinates);
+        this.setState(state => ({ alerts: [alert] }));
+      };
     };
 
     client.onclose = () => {
@@ -25,7 +59,12 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        Alerting Dashboard Version 2 <br />
+        <div>
+          Alerting Dashboard Version 2 <br />
+        </div>
+        <div>
+          Alerts: {this.state.alerts}
+        </div>
       </div>
     )
   };
