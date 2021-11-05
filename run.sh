17d84caad836576362362fa2074d3b2d98ffc1c9
Folkerth, Jennifer <jennifer.folkerth@accenture.com>
2021-11-05 14:09:03 -0400

    Many changes to try to get ui to point to proper environment, kludging in production values for now

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#cat > /usr/share/nginx/html/config.js <<EOL
+#window.ALERTS_URL = '${ALERTS_URL}'
+#window.FEEDBACK_URL = '${FEEDBACK_URL}'
+#EOL
+
 cat /etc/nginx/conf.d/default.conf.tpl | envsubst \$ADDITIONAL_CSP_HOSTS > /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
